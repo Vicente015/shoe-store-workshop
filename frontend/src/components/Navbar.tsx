e9a9af535a7bdf0622db723f4aa71c33b0984d3a
Jose Cabrera <jose@codium.team>
2024-07-23 11:39:36 +0200

    Add min-width to links

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,9 +8,9 @@ export function Navbar() {
 
   return (
     <nav className='bg-white shadow-lg'>
-      <div className='md:flex items-center justify-between py-2 px-8 md:px-12'>
+      <div className='flex items-center justify-between py-2 px-8 md:px-12'>
         <div className='container mx-auto mt-2 flex justify-between items-center'>
-          <div className='text-2xl font-bold text-gray-800 md:text-3xl'>
+          <div>
             <a href='/'>
               <img
                 alt={'Codium logo'}
@@ -21,26 +21,12 @@ export function Navbar() {
               />
             </a>
           </div>
-          <div className='md:hidden'>
-            <button
-              type='button'
-              className='block text-gray-800 hover:text-gray-700 focus:text-gray-700 focus:outline-none'
-            >
-              <svg className='h-6 w-6 fill-current' viewBox='0 0 24 24'>
-                <path
-                  className='hidden'
-                  d='M16.24 14.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 0 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12l2.83 2.83z'
-                />
-                <path d='M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z' />
-              </svg>
-            </button>
-          </div>
         </div>
-        <div className='flex flex-col md:flex-row md:block -mx-2'>
+        <div className='flex flex-row -mx-2'>
           <Link
             id={'cart-link'}
             to='/cart'
-            className='text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100  py-2 px-2 md:mx-2 relative'
+            className='min-w-16 text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100  py-2 px-2 md:mx-2 relative'
           >
             <span>Cart</span>
             <span
@@ -57,7 +43,7 @@ export function Navbar() {
           {!email && (
             <Link
               to='/login'
-              className='text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100  py-2 px-2 md:mx-2'
+              className='min-w-16 text-gray-800 rounded hover:bg-gray-900 hover:text-gray-100  py-2 px-2 md:mx-2'
             >
               Log in
             </Link>
