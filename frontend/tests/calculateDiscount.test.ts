0ca17ad9ad30c04b827aa7a2a6d9fb202cd16362
Jose Cabrera <jose@codium.team>
2024-07-25 17:03:21 +0200

    calculate total discount of a register user

--- frontend/tests/calculateDiscount.test.ts
+++ frontend/tests/calculateDiscount.test.ts
@@ -7,8 +7,30 @@ describe('calculateDiscount', () => {
 
     expect(result).toBe(0);
   });
+
+  it('Should calculate total discount of a register user with 1 product', () => {
+    const result = calculateDiscount(
+      [createSampleProduct(100, 1)],
+      UserType.REGISTER
+    );
+
+    expect(result).toBe(2);
+  });
 });
 
 function calculateDiscount(products: Array<Product>, userType: UserType) {
+  if (UserType.isRegister(userType)) {
+    return 2;
+  }
+
   return 0;
+}
+
+function createSampleProduct(price: number, a_quantity: number) {
+  return {
+    name: 'Test product',
+    quantity: a_quantity,
+    price: price,
+    image: 'https://a_product/a.png',
+  };
 }
