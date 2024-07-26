37c1ae75f25e9451006e6017c779db5d13072b6c
Jose Cabrera <jose@codium.team>
2024-07-26 12:10:26 +0200

    add it to button

--- frontend/src/pages/Payment.tsx
+++ frontend/src/pages/Payment.tsx
@@ -130,6 +130,7 @@ export function PaymentPage() {
                 </div>
 
                 <button
+                  id={'pay-now'}
                   type='submit'
                   className='flex w-full items-center justify-center rounded-lg bg-gray-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-gray-800 focus:outline-none focus:ring-4  focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800'
                 >
