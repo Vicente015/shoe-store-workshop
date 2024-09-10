fe810da8f325739410cda39417ce1c09bd956a51
Hugo Chinchilla <hugo@codium.team>
2024-09-10 10:40:04 +0200

    test happy path creates an Order

--- backend/tests/Feature/CheckoutControllerTest.php
+++ backend/tests/Feature/CheckoutControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Order;
 use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\Attributes\Test;
@@ -27,5 +28,29 @@ class CheckoutControllerTest extends TestCase
         ]);
 
         $response->assertStatus(200);
+    }
+
+    #[Test]
+    public function test_checkout_creates_an_order(): void
+    {
+        $prodcut_to_purchase = Product::create([
+            'name' => 'Nike Zoom',
+            'price' => 50,
+            'image' => 'https://picsum.photos/201/300',
+        ]);
+        $prodcut_to_purchase->save();
+
+        $this->postJson('/api/checkout', [
+            'price' => 50.0,
+            'products' => ['nike-zoom'],
+            'email' => 'example@example.com',
+        ]);
+
+        $this->assertEquals(1, Order::query()->count());
+        $order = Order::query()->first();
+        $this->assertEquals($order->user, null);
+        $this->assertEquals($order->price, 50);
+        $this->assertCount(1, $order->products);
+        $this->assertTrue($order->products[0]->is($prodcut_to_purchase));
     }
 }
